c63930a769f2595b868f2affbe7d18bc30a8918d
Mario Pohrib <m9@Mario-MacBook-Air.local>
2024-12-10 18:48:55 +0100

    Fixed the appointment logic but needs more flexible functionality

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -6,25 +6,14 @@
 
 using namespace std;
 
-
-class Test{
-    public:
-    int num = 10;//Just testing 
-};
-
 class Patient{
     public:
     string name;
     string medicalHistory;
     string address;
-    bool hasAppointment;
-    
-    //Patient(string name, string address){
-        //this->name = name;
-        //this->address = address;
-    
-    //}
-
+    string appointment = ""; //Does this work for c++ ?
+    map<int,bool>hasAppointment;
+   
     crow::json::wvalue to_json() const {
         crow::json::wvalue j;
         j["name"] = name;
@@ -51,23 +40,58 @@ class Patient{
     //}
 //};
 
-struct Appointment {
-    int pid;
-    string date; // Day Month Year
-    string time; // Hours: Minutes
-    
+class DoctorClass{
+    public: 
+    int docId;
+    bool isAvailable;
+    //docId is mapped to the timeslots 
+    map<int,string>slots;
 
     crow::json::wvalue to_json() const {
         crow::json::wvalue j;
-        j["pid"] = pid;
-        j["date"] = date;
-        j["time"] = time;
-        // j["doctor"] = doctor;
+        j["ID"] = docId;
         return j;
-    }
+    } 
 };
 
+class AppointmentClass{
+
+    //To book an appointment, map pid and docId
+    //In the doctorclass, the following timeslot is mapped to docID 
+    public:
+    map<int,int>patientToDocMap;
+    vector<string>availableTimeSlots = {"9:00","9:30","10,00,","10:30"};
+    //crow::json::wvalue to_json() const {
+        //crow::json::wvalue j;
+        //j["pid"] = pid;
+        //j["date"] = date;
+        //j["time"] = time;
+        // j["doctor"] = doctor;
+        //return j;
+    //}
+   
+};
+
+//struct Appointment {
+
+   // int pid;
+   // string date; // Day Month Year
+   //string time; // Hours: Minutes
+    
+
+    //crow::json::wvalue to_json() const {
+        //crow::json::wvalue j;
+        //j["pid"] = pid;
+        //j["date"] = date;
+        //j["time"] = time;
+         //j["doctor"] = doctor;
+        //return j;
+    //}
+//};
+
 struct Doctor {
+
+    
     string name;
     string specialization;
 
@@ -83,14 +107,16 @@ int main() {
     crow::SimpleApp app;
 
     map<int, Patient> patients;                
-    map<int, vector<Appointment>> appointments; 
-    map<int, Doctor> doctors;                  
+    //map<int, vector<Appointment>> appointments; 
+    //map<int, Doctor> doctors; 
+    map<int,DoctorClass>doctorsMap;        
     int patientCounter = 0;
     int doctorCounter = 0;  
 
-    // to register patient 
+    // to register a patient 
     // address:port/register?name=whatever whatever&street= 12312313 anywhere
     //  patientCounter automatically increases by 1 when new patients are created
+    //The patient can not have an appointment at the time of the registration
     CROW_ROUTE(app, "/register").methods("GET"_method)([&](const crow::request& req) {
         if (!req.url_params.get("name") || !req.url_params.get("address")) {
             return crow::response(404, "Missing 'name' or 'address' query parameter.");
@@ -104,13 +130,13 @@ int main() {
         patientObject.address = address;
 
         //Patient patientObject{name, address};
-        //Save patients data to a JSON file 
+        //Save patients data to a JSON file for now  
 
         crow::json::wvalue jsonRes;
 
         patients[patientCounter] = patientObject;
-
-        jsonRes[std::to_string(patientCounter)] = patientObject.to_json();
+        patientObject.hasAppointment[patientCounter] = false;//No appointment 
+        jsonRes[std::to_string(patientCounter)] = patientObject.to_json(); 
         patientCounter++;
 
         return crow::response(jsonRes);
@@ -125,13 +151,16 @@ int main() {
 
         string name = req.url_params.get("name");
         string specialization = req.url_params.get("specialization");
+        //Doctor doctorObject{name, specialization};//struct
 
-        Doctor doctorObject{name, specialization};
+        DoctorClass doctor;
+        doctor.docId = doctorCounter++;
+        doctor.isAvailable = true;//Initially they are free
         crow::json::wvalue jsonRes;
-
-        doctors[doctorCounter] = doctorObject; // Add doctor
-        jsonRes[std::to_string(doctorCounter)] = doctorObject.to_json();
-        doctorCounter++; // Increment doctor ID
+        // Add doctor to the map and save this to a JSON file
+        doctorsMap[doctor.docId] = doctor; 
+        jsonRes[std::to_string(doctorCounter)] = doctor.to_json();
+        //doctorCounter++; // Increment doctor ID
 
         return crow::response(jsonRes);
     });
@@ -147,61 +176,63 @@ int main() {
         string date = req.url_params.get("date");
         string time = req.url_params.get("time");
 
-        if (patients.find(pid) == patients.end()) {
-            return crow::response(404, "Patient not found");
-        }
+        //if (patients.find(pid) == patients.end()) {
+            //return crow::response(404, "Patient not found");
+        //}
 
-        Appointment appointment{pid, date, time};
-        appointments[pid].push_back(appointment);
+        //Loop through the doctors map and associate docId and pid, 
+        //then associate pid, docId with the timeslot, removing the already-taken timeslot from  the vector  
+        AppointmentClass book;
+        DoctorClass doctorObj;
+        Patient patientBookAppo;
+        int tempContainer;//Doctor id holder
+        for(const auto & doctor:doctorsMap){
+            tempContainer = doctor.first;
+            book.patientToDocMap[pid] = tempContainer;
+           
+        } 
+        //cout<<"Doc id";
+        //cout<<book.patientToDocMap[pid];
+        //Booking an appointment 
+        for(int i = 0; i < book.availableTimeSlots.size();i++){
+
+            patientBookAppo.appointment = book.availableTimeSlots[0];//vector needs to be re-initialised
+            doctorObj.slots[tempContainer] = patientBookAppo.appointment;
+            if(patientBookAppo.hasAppointment[pid] == false){
+               patientBookAppo.hasAppointment[pid] = true; 
+               cout<<"The patient has an appointment \b";
+               cout<<patientBookAppo.appointment;
+            }
+        }
+        
+
+        //Appointment appointment{pid, date, time};
+        //appointments[pid].push_back(appointment);
 
         crow::json::wvalue response;
+        //response["message"] = "Appointment booked successfully";
+        //response["appointment"] = appointment.to_json();
         response["message"] = "Appointment booked successfully";
-        response["appointment"] = appointment.to_json();
         return crow::response(response);
     });
 
     // address:port/getappointment/pid will get all the appointments that patient has
-    CROW_ROUTE(app, "/getappointments/<int>")([&](int pid) {
-        if (patients.find(pid) == patients.end()) {
-            return crow::response(404, "Patient ID is incorrect or there is no such patient");
-        }
+    //CROW_ROUTE(app, "/getappointments/<int>")([&](int pid) {
+       // if (patients.find(pid) == patients.end()) {
+            //return crow::response(404, "Patient ID is incorrect or there is no such patient");
+        //}
 
-        crow::json::wvalue response;
-        vector<crow::json::wvalue> appointmentList;
+        //crow::json::wvalue response;
+        //vector<crow::json::wvalue> appointmentList;
 
-        for (const auto& appointment : appointments[pid]) {
-            appointmentList.push_back(appointment.to_json());
-        }
+        //for (const auto& appointment : appointments[pid]) {
+            //appointmentList.push_back(appointment.to_json());
+        //}
 
-        response["appointments"] = std::move(appointmentList);
-        return crow::response(response);
-    });
+        //response["appointments"] = std::move(appointmentList);
+        //return crow::response(response);
+    //});
 
-    // list of all doctors address:port/doctors
-    CROW_ROUTE(app, "/doctors").methods("GET"_method)([&]() {
-        crow::json::wvalue response;
-        vector<crow::json::wvalue> doctorList;
-
-        for (const auto& doctor : doctors) {
-            doctorList.push_back(doctor.second.to_json());
-        }
-
-        response["doctors"] = std::move(doctorList);
-        return crow::response(response);
-    });
-
-    // list of all patients address:port/patients
-    CROW_ROUTE(app, "/patients").methods("GET"_method)([&]() {
-        crow::json::wvalue response;
-        vector<crow::json::wvalue> patientList;
-
-        for (const auto& patient : patients) {
-            patientList.push_back(patient.second.to_json());
-        }
-
-        response["patients"] = std::move(patientList);
-        return crow::response(response);
-    });
 
     app.port(3333).run();
     return 0;
